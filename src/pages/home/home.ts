695fd9509b1b21dd272a4f03475da94de03c162d
Marcos Paulo Campos <marcospcamposfilho@gmail.com>
2018-12-14 09:36:39 -0200

    Finalização do Curso
    Terminadas as lectures 5 e 6 do curso Ionic Basics

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { isUndefined } from 'ionic-angular/umd/util/util';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  users: any[];
+  weatherInfo: any = {};
 
+  cityName: string = "London";
+
+  constructor(
+    public navCtrl: NavController,
+    public http: Http,
+    public loading: LoadingController
+  ) {
   }
 
+
+  getData() {
+    let load = this.loading.create();
+    load.present();
+    this.http.get("http://api.openweathermap.org/data/2.5/weather?q=" + this.cityName + "&appId=9ebe10ea68cddaa7da9a273c5ace3048").subscribe((data) => {
+      this.weatherInfo = data.json();
+      console.log(data.json());
+      
+      load.dismiss();
+    }, (error) => {
+      load.dismiss();
+    });
+
+    // /* a função subscribe registra os handlers ou o callback que serão executadas quando
+    //   a requisição HTTP for feita (com sucesso e com erro) */
+    // this.http.get("http://jsonplaceholder.typicode.com/users").subscribe((data) => {
+    //   console.log(data.json());
+    //   this.users = data.json();
+    // }, (error) => {
+    //   console.log(error);
+    // });
+  }
 }
